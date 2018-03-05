ee275a1cab1c4190d73f91a70b11e5c058d46f4e
auroracordes <corde171@morris.umn.edu>
2018-03-05 16:33:00 -0600

    Add methods to goals.component.ts to give it functionality.

--- client/src/app/goals/goals.component.ts
+++ client/src/app/goals/goals.component.ts
@@ -5,10 +5,11 @@ import {MatDialog} from '@angular/material';
 import {Goal} from './goal'
 
 @Component({
-    selector: 'goals-component',
+    selector: 'app-goals-component',
     templateUrl: 'goals.component.html',
     styleUrls: ['./goals.component.css'],
 })
+
 export class GoalsComponent implements OnInit {
     public title: string;
 
@@ -23,11 +24,67 @@ export class GoalsComponent implements OnInit {
     public goalGoal: string;
     public goalTimeCreated: number;
     public goalComplete: boolean;
+    public user_id: number;
 
+   /* openDialog(): void {
+        const newGoal: Goal = {_id: '', user_id: -1, goal: '', timeCreated: -1, complete: false};
+        const dialogRef = this.dialog.open()
+    }*/
 
+   refreshGoals(): Observable<Goal[]> {
+       const goalObservable: Observable<Goal[]> = this.goalService.getGoals();
+       goalObservable.subscribe(
+           goals => {
+               this.goals = goals;
+               this.filterGoals(this.goalGoal, this.goalTimeCreated, this.goalComplete);
+           },
+           err => {
+               console.log(err);
+           });
+       return goalObservable;
+   }
+
+   public filterGoals(searchGoal: string, searchTime: number, searchComplete: boolean) {
+       this.filteredGoals = this.goals;
+
+       if(searchGoal != null) {
+           searchGoal = searchGoal.toLocaleLowerCase();
+
+           this.filteredGoals = this.filteredGoals.filter(goal => {
+               return !searchGoal || goal.goal.toLowerCase().indexOf(searchGoal) !== -1;
+           });
+       }
+
+       if(searchTime != null) {
+           this.filteredGoals = this.filteredGoals.filter(goal => {
+               return !searchTime || goal.timeCreated == searchTime;
+           });
+       }
+
+       if(searchComplete != null) {
+           this.filteredGoals = this.filteredGoals.filter(goal => {
+               return null || goal.complete == searchComplete;
+           });
+       }
+
+       return this.filteredGoals;
+
+   }
+
+   loadService(): void {
+       this.goalService.getGoals(this.user_id).subscribe(
+           goals => {
+               this.goals = goals;
+               this.filteredGoals = this.goals;
+           },
+           err => {
+               console.log(err);
+           }
+       );
+   }
 
     ngOnInit(): void {
-        // this.refreshGoals();
-        // this.loadService();
+        this.refreshGoals();
+        this.loadService();
     }
 }
